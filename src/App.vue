8696c8251a14d1669af69425835c1b5ce26d5994
sad1go0 <sayidullohabbasov0909@gmail.com>
2022-12-24 01:04:53 +0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <Header />
+  <div class="container mx-auto">
+    <Home />
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue';
+import Home from './views/Home.vue';
+
+export default {
+  components: {
+    Header,
+    Home,
+  },
+};
+</script>
